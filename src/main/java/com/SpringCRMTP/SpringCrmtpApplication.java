37b525f429376792d99d3ef0b8ac5185bbd16625
Saafet fadwa <saafetfadwa@gmail.com>
2023-03-02 00:35:35 -0800

    first version

---
+++ src/main/java/com/SpringCRMTP/SpringCrmtpApplication.java
@@ -0,0 +1,13 @@
+package com.SpringCRMTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrmtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrmtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringCRMTP/SpringCrmtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringCRMTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrmtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
